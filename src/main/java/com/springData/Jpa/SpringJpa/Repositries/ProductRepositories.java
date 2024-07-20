24f5cb39dae79b3d37e9cf98a57aa9b2020c5a73
Shivam Singh <shivamfaceuit@gmail.com>
2024-07-20 12:45:15 +0530

    done with the lec 3.3

--- src/main/java/com/springData/Jpa/SpringJpa/Repositries/ProductRepositories.java
+++ src/main/java/com/springData/Jpa/SpringJpa/Repositries/ProductRepositories.java
@@ -2,9 +2,19 @@ package com.springData.Jpa.SpringJpa.Repositries;
 
 import com.springData.Jpa.SpringJpa.entites.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepositories extends JpaRepository<Products,Long> {
 
+
+    List<Products> findByQuantityGreaterThanOrPriceLessThan(int quantity, BigDecimal price);
+
+    @Query("select e from Products  e where e.title?1 and e.price?2")
+    Optional<Products>findByTitleAndPrice(String title ,BigDecimal price);
 }
--- src/test/java/com/springData/Jpa/SpringJpa/SpringJpaApplicationTests.java
+++ src/test/java/com/springData/Jpa/SpringJpa/SpringJpaApplicationTests.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
+
 @SpringBootTest
 class SpringJpaApplicationTests {
 	@Autowired
@@ -23,7 +25,7 @@ class SpringJpaApplicationTests {
 				.sku("test124")
 				.price(BigDecimal
 						.valueOf(123.67))
-				.quantity(34)
+				.quantity(4)
 				.build();
 		Products saveProduct=productRepositories.save(products);
 		System.out.println(saveProduct);
@@ -31,8 +33,20 @@ class SpringJpaApplicationTests {
 
 	@Test
 	void  getRepo(){
-		List<Products> entites=productRepositories.findAll();
-		System.out.println(entites);
+//		List<Products> entites=productRepositories.findAll();
+
+//		System.out.println(entites);
+				List<Products> entities = productRepositories.findByQuantityGreaterThanOrPriceLessThan(4, BigDecimal.valueOf(23.45));
+		System.out.println(entities);
+
 	}
+
+	@Test
+	void getSingleFromRepository() {
+		Optional<Products> productEntity = productRepositories
+				.findByTitleAndPrice("Peps", BigDecimal.valueOf(14.4));
+		productEntity.ifPresent(System.out::println);
+	}
+
 
 }
